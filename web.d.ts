8447a62e9cee6ca422f4c297c1333d5c50ea36a6
zerkalica <zerkalica@users.noreply.github.com>
2020-09-11 07:01:57 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.d.ts
+++ web.d.ts
@@ -1143,7 +1143,7 @@ declare namespace $ {
                 blur: Length;
                 spread: Length;
                 color: Color;
-            }[];
+            }[] | 'none' | Common;
         };
         font?: {
             style?: 'normal' | 'italic' | Common;
