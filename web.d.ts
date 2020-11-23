2f9174c38f36600a9a07ccf25139058c1b9a829d
nin-jin <nin-jin@users.noreply.github.com>
2020-11-23 07:15:01 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.d.ts
+++ web.d.ts
@@ -1437,6 +1437,10 @@ declare namespace $ {
 }
 
 declare namespace $ {
+    function $mol_support_css_overflow_anchor(this: $mol_ambient_context): boolean;
+}
+
+declare namespace $ {
     class $mol_dom_listener extends $mol_object {
         _node: any;
         _event: string;
