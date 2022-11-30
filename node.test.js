494fe15f705d1c7d8232d0eb3b5686da0ac198a7
nin-jin <nin-jin@users.noreply.github.com>
2022-11-30 07:10:07 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -2986,8 +2986,7 @@ var $;
         event() {
             return {
                 ...super.event(),
-                input: (event) => this.event_change(event),
-                keydown: (event) => this.event_key_press(event)
+                input: (event) => this.event_change(event)
             };
         }
         plugins() {
@@ -3043,11 +3042,6 @@ var $;
                 return event;
             return null;
         }
-        event_key_press(event) {
-            if (event !== undefined)
-                return event;
-            return null;
-        }
         submit(event) {
             if (event !== undefined)
                 return event;
@@ -3073,9 +3067,6 @@ var $;
     __decorate([
         $mol_mem
     ], $mol_string.prototype, "event_change", null);
-    __decorate([
-        $mol_mem
-    ], $mol_string.prototype, "event_key_press", null);
     __decorate([
         $mol_mem
     ], $mol_string.prototype, "submit", null);
