6830bcb538574ead67b511a4770544f7b1ded188
nin-jin <nin-jin@users.noreply.github.com>
2025-08-22 07:11:42 +0000

    Deploying to gh-pages from @ hyoo-ru/todomvc.hyoo.ru@fa2419a52088c79184f99d8343576fc22c29e647 🚀

--- web.d.ts
+++ web.d.ts
@@ -338,6 +338,10 @@ declare namespace $ {
 }
 
 declare namespace $ {
+    function $mol_try<Result>(handler2: () => Result): Result | Error;
+}
+
+declare namespace $ {
     function $mol_fail_log(error: unknown): boolean;
 }
 
