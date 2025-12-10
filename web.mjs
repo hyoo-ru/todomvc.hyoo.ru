bc23f783198c85451b7df35e5359aa4b324c2252
nin-jin <nin-jin@users.noreply.github.com>
2025-12-10 07:15:37 +0000

    Deploying to gh-pages from @ hyoo-ru/todomvc.hyoo.ru@fa2419a52088c79184f99d8343576fc22c29e647 🚀

--- web.mjs
+++ web.mjs
@@ -444,6 +444,8 @@ var $;
 "use strict";
 var $;
 (function ($) {
+    if (!Symbol.dispose)
+        Symbol.dispose = Symbol('Symbol.dispose');
     class $mol_object2 {
         static $ = $;
         [Symbol.toStringTag];
@@ -476,6 +478,9 @@ var $;
         }
         destructor() { }
         static destructor() { }
+        [Symbol.dispose]() {
+            this.destructor();
+        }
         toString() {
             return this[Symbol.toStringTag] || this.constructor.name + '<>';
         }
