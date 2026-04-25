c1e06e143c5abcc0c425f8aec2c2d3e9d4decb76
nin-jin <nin-jin@users.noreply.github.com>
2026-04-25 08:00:01 +0000

    Deploying to gh-pages from @ hyoo-ru/todomvc.hyoo.ru@fa2419a52088c79184f99d8343576fc22c29e647 🚀

--- node.test.js
+++ node.test.js
@@ -2414,6 +2414,8 @@ var $;
         'field',
         'image',
         'spirit',
+        'hue',
+        'hue_spread',
     ]);
 })($ || ($ = {}));
 
@@ -4650,7 +4652,7 @@ var $;
                         }
                     }
                 }
-                else if (key === '@media') {
+                else if (key === '@media' || key === '@container') {
                     const media = config[key];
                     for (let query of Object.keys(media).reverse()) {
                         rules.push('}\n');
